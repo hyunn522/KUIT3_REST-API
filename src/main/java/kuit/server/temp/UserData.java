af60ed3ed6423fe416f07449b7e2cf074a91cbc8
hyunn522 <tjgus52213@gmail.com>
2024-05-29 22:17:06 +0900

    feat: response test

--- src/main/java/kuit/server/temp/UserData.java
+++ src/main/java/kuit/server/temp/UserData.java
@@ -1,10 +1,12 @@
 package kuit.server.temp;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserData {
     private String username;
     private int age;
